a33b64137096161f25e0f0a4f423c29c60045cab
Sebastian <fase22290@icloud.com>
2023-12-30 13:50:12 +0100

    p1

--- go/main.go
+++ go/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"aoc/rowmapper"
+	"aoc/aoc5"
 	"fmt"
 )
 
 func main() {
 
-	mapper := rowmapper.MapRow()
+	seeds := []int{
+		2019933646, 2719986, 2982244904, 337763798, 445440, 255553492, 1676917594, 196488200, 3863266382, 36104375, 1385433279, 178385087, 2169075746, 171590090, 572674563, 5944769, 835041333, 194256900, 664827176, 42427020,
+	}
+
+	mapper := aoc5.GetLocation(seeds)
 	fmt.Println(mapper)
 }
